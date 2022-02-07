5b6547dbac1e88354415aeeff6f8778a9b76a741
udden89 <udd.marcus@gmail.com>
2022-02-07 18:59:32 +0100

    start

---
+++ soundify-api/src/main/java/com/soundify/api/soundifyapi/SoundifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.soundify.api.soundifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundifyApiApplication.class, args);
+	}
+
+}
---
+++ soundify-api/src/test/java/com/soundify/api/soundifyapi/SoundifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundify.api.soundifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
