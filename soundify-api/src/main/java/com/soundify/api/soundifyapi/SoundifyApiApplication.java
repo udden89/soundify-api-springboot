8cf451bf92b8f494649ae7f0a6c2125edb6ba120
udden89 <udd.marcus@gmail.com>
2022-02-09 12:02:25 +0100

    Added .env

--- soundify-api/src/main/java/com/soundify/api/soundifyapi/SoundifyApiApplication.java
+++ soundify-api/src/main/java/com/soundify/api/soundifyapi/SoundifyApiApplication.java
@@ -1,13 +1,18 @@
 package com.soundify.api.soundifyapi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SoundifyApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SoundifyApiApplication.class, args);
+
+
 	}
 
 }
