6138169f6b5c4952bb09ca3c3b1626bd47204b2e
Dannemaniman <daniellangas91@gmail.com>
2022-02-16 01:43:10 +0100

    Implemented route and service that calls youtube API via WebClient.

--- soundify-api/src/main/java/com/soundify/api/soundifyapi/controller/SearchController.java
+++ soundify-api/src/main/java/com/soundify/api/soundifyapi/controller/SearchController.java
@@ -1,48 +1,27 @@
 package com.soundify.api.soundifyapi.controller;
-
-import com.soundify.api.soundifyapi.model.Song;
-import com.soundify.api.soundifyapi.model.User;
 import com.soundify.api.soundifyapi.service.SearchService;
-import com.soundify.api.soundifyapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api")
 public class SearchController {
 
-    private final SearchService searchService;
-
     @Autowired
-    public SearchController(SearchService searchService) {
-        this.searchService = searchService;
+    SearchService searchService;
+
+    @GetMapping("/{mediaType}")
+    public String searchMusicAPI(@RequestParam(name="query") String query,
+                                 @RequestParam(name="next") Optional<String> next,
+                                 @RequestParam(name="mediaType") Optional<String> mediaType) {
+
+        String type = mediaType.isPresent() ? mediaType.get() : "search";
+        String nextQuery = next.isPresent() ? "?next=" + next : "";
+        String result = searchService.fetch(type, query, nextQuery);
+
+        return result;
     }
-
-    @GetMapping("/search")
-    public void getAllUsers() {
-
-        return ;
-    }
-
-
- /*
-    router.get('/:mediaType', async (req: Request, res: Response) => {
-
-  const mediaType = req.params.mediaType
-  const query = req.query.query
-  const next = req.query?.next
-
-  const result = await searchService(mediaType, query, next)
-
-        if (result) {
-            return res.json(result)
-        } else {
-            return res.sendStatus(500).json(result.message)
-        }
-    })*/
-
-
 }
--- soundify-api/src/main/java/com/soundify/api/soundifyapi/service/SearchService.java
+++ soundify-api/src/main/java/com/soundify/api/soundifyapi/service/SearchService.java
@@ -1,7 +1,32 @@
 package com.soundify.api.soundifyapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class SearchService {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    public String fetch(String mediaType, String query, String next) {
+        return getFromHeroku(mediaType, query, next);
+    }
+
+    public String getFromHeroku(String mediaType, String query, String next) {
+        String path = "https://yt-music-api.herokuapp.com/api/yt/" + mediaType + "/";
+        path = path + query + next  ;
+
+        String responseJson = webClientBuilder.build()
+                .get()
+                .uri(path)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return responseJson;
+    }
+
+
 }
